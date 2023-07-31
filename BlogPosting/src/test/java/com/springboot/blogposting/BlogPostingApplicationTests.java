942591e67cde0ca587350a9d4b2055e717133e1b
RamAnanth11 <ananthram119@gmail.com>
2023-08-01 01:24:31 +0530

    Initial Uploads

---
+++ BlogPosting/src/test/java/com/springboot/blogposting/BlogPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blogposting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
